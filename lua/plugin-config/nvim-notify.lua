97d82b2b8ae3d007f6c0b98d3cab587d9fd1b6a6
horaoen <horaoen@gmail.com>
2023-01-07 23:56:02 +0800

    fix nvim-notify

--- lua/plugin-config/nvim-notify.lua
+++ lua/plugin-config/nvim-notify.lua
@@ -1,11 +1,15 @@
 return function ()
     local exist, notify = pcall(require, "notify")
+    
     if not exist then
         return
     end
+
+    vim.opt.termguicolors = true
 
     notify.setup({
         stages = "fade_in_slide_out",
         timeout = 3000,
     })
+    vim.notify = notify
 end
