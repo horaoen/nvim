854e4957262fa19197b7dda3ed326a0e9bbc8096
horaoen <horaoen@gmail.com>
2023-06-29 18:41:16 +0800

    update for java

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,21 @@
 local config = {
-    cmd = {'jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    cmd = {
+        'jdtls',
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xmx1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens',
+        'java.base/java.util=ALL-UNNAMED',
+        '--add-opens',
+        'java.base/java.lang=ALL-UNNAMED',
+    },
+    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+    
 }
 require('jdtls').start_or_attach(config)
+require('jdtls.setup').add_commands()
