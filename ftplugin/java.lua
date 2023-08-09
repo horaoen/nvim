8a6e7708569fd5fcf68f5a8813808d12f195eccc
horaoen <horaoen@gmail.com>
2023-08-09 16:14:19 +0800

    config nvim-jdtls for lombok

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,21 +1,15 @@
+local jdtls_path = fn.stdpath('data') .. '/mason/share/jdtls'
+local lombok_path = jdtls_path .. '/lombok.jar'
 local config = {
-    cmd = {
-        'jdtls',
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-Xmx1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens',
-        'java.base/java.util=ALL-UNNAMED',
-        '--add-opens',
-        'java.base/java.lang=ALL-UNNAMED',
+    cmd = { 'jdtls', '--jvm-arg=-javaagent:' .. lombok_path },
+    root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew', 'pom.xml' }),
+    settings = {
+        java = {},
     },
-    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-    
+
+    init_options = {
+        bundles = {},
+    },
 }
 require('jdtls').start_or_attach(config)
 require('jdtls.setup').add_commands()
