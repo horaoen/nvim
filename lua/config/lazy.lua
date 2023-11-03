733f5becc2dc54bd4e2ccdffa3bda655ec99f779
horaoen <horaoen@gmail.com>
2023-11-03 22:23:23 +0800

    update lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
-    { "LazyVim/LazyVim",                                       import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.json" },
 
     -- language
