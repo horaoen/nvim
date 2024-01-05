b88fd0207866cfef1d13ef899500a90ddb3b6044
horaoen <horaoen@gmail.com>
2024-01-05 17:13:08 +0800

    refactor global keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-keyset("i", "jk", "<ESC>")
-keyset("n", "<leader>w", "<cmd>w<cr>")
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+map("i", "jk", "<ESC>")
+map("n", "<leader>w", "<cmd>w<cr>")
