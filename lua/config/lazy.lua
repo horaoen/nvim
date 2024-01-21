450fbc52a935b9eaa5594e8e6d4d657c633ed899
horaoen <horaoen@gmail.com>
2024-01-21 17:47:54 +0800

    update lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,33 +10,35 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-    spec = {
-        {"LazyVim/LazyVim", import = "lazyvim.plugins"},
-        {import = "lazyvim.plugins.extras.lang.json"}, -- language
-        {import = "lazyvim.plugins.extras.lang.clangd"},
-        {import = "lazyvim.plugins.extras.lang.cmake"},
-        {import = "lazyvim.plugins.extras.lang.rust"},
-        {import = "lazyvim.plugins.extras.lang.json"},
-        {import = "lazyvim.plugins.extras.lang.docker"},
-        {import = "lazyvim.plugins.extras.lang.markdown"},
-        {import = "lazyvim.plugins.extras.lang.yaml"}, -- extras
-        {import = "lazyvim.plugins.extras.util.project"},
-        {import = "lazyvim.plugins.extras.ui.edgy"},
-        {import = "lazyvim.plugins.extras.coding.copilot"}, -- editor
-        {import = "lazyvim.plugins.extras.coding.yanky"}, {import = "plugins"}
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.json" }, -- language
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.yaml" }, -- extras
+    { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.coding.copilot" }, -- editor
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false, -- always use the latest git commit
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip", -- "matchit",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
     },
-    defaults = {
-        lazy = false,
-        version = false -- always use the latest git commit
-    },
-    install = {colorscheme = {"tokyonight", "habamax"}},
-    checker = {enabled = true}, -- automatically check for plugin updates
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                "gzip", -- "matchit",
-                "tarPlugin", "tohtml", "tutor", "zipPlugin"
-            }
-        }
-    }
+  },
 })
