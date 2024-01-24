c8e6a29366ad62e1cf998208171abef90e521649
horaoen <horaoen@gmail.com>
2024-01-24 20:54:52 +0800

    format

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,54 +1,60 @@
 return {
-    "nvimdev/dashboard-nvim",
-    opts = {
-        theme = "hyper",
-        config = {
-            header = {
-                "", "",
-                "        ⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ",
-                "      ⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷ ",
-                "     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ ",
-                "    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ ",
-                "  ⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿ ",
-                " ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿ ",
-                " ⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ",
-                " ⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ",
-                " ⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ ",
-                " ⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ",
-                " ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ ",
-                " ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ ",
-                " ⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ ",
-                "  ⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋  ⣾⡌⢠⣿⡿⠃ ",
-                " ⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉            ",
-                "", "", " practice makes perfect.", ""
-            },
-            weak_header = {enable = false},
-            shortcut = {
-                {
-                    desc = "󰊳 Update",
-                    group = "@property",
-                    action = "Lazy update",
-                    key = "u"
-                }, {
-                    icon = " ",
-                    icon_hl = "@variable",
-                    desc = "Files",
-                    group = "Label",
-                    action = "Telescope find_files",
-                    key = "f"
-                }, {
-                    desc = " config",
-                    group = "Number",
-                    action = "lua require('lazyvim.util').telescope.config_files()()",
-                    key = "c"
-                },
-                {
-                    desc = " quit",
-                    group = "DiagnosticHint",
-                    action = "qa",
-                    key = "q"
-                }
-            }
-        }
-    }
+  "nvimdev/dashboard-nvim",
+  opts = {
+    theme = "hyper",
+    config = {
+      header = {
+        "",
+        "",
+        "        ⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ",
+        "      ⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷ ",
+        "     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ ",
+        "    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ ",
+        "  ⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿ ",
+        " ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿ ",
+        " ⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ",
+        " ⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ",
+        " ⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ ",
+        " ⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ",
+        " ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ ",
+        " ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ ",
+        " ⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ ",
+        "  ⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋  ⣾⡌⢠⣿⡿⠃ ",
+        " ⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉            ",
+        "",
+        "",
+        " practice makes perfect.",
+        "",
+      },
+      weak_header = { enable = false },
+      shortcut = {
+        {
+          desc = "󰊳 Update",
+          group = "@property",
+          action = "Lazy update",
+          key = "u",
+        },
+        {
+          icon = " ",
+          icon_hl = "@variable",
+          desc = "Files",
+          group = "Label",
+          action = "Telescope find_files",
+          key = "f",
+        },
+        {
+          desc = " config",
+          group = "Number",
+          action = "lua require('lazyvim.util').telescope.config_files()()",
+          key = "c",
+        },
+        {
+          desc = " quit",
+          group = "DiagnosticHint",
+          action = "qa",
+          key = "q",
+        },
+      },
+    },
+  },
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,47 +1,47 @@
 return {
-    "neovim/nvim-lspconfig",
-    init = function()
-        local keys = require("lazyvim.plugins.lsp.keymaps").get()
-        -- change a keymap
-        keys[#keys + 1] = {"<leader>ca", "<cmd>Lspsaga code_action<cr>"}
-        keys[#keys + 1] = {"K", "<cmd>Lspsaga hover_doc<cr>"}
-        keys[#keys + 1] = {
-            "<leader>cr",
-            "<cmd>Lspsaga rename<cr>",
-            desc = "Rename"
-        }
-        keys[#keys + 1] = {
-            "gd",
-            "<cmd>Lspsaga goto_definition<cr>",
-            desc = "Goto Definition"
-        }
-        keys[#keys + 1] = {
-            "gy",
-            "<cmd>Lspsaga goto_type_definition<cr>",
-            desc = "Goto Type Definition"
-        }
-    end,
-    ---@class PluginLspOpts
-    opts = {
-        inlay_hints = {enabled = false},
-        ---@type lspconfig.options
-        servers = {
-            jsonls = {},
-            bashls = {},
-            typst_lsp = {},
-            lua_ls = {
-                -- mason = false, -- set to false if you don't want this server to be installed with mason
-                -- Use this to add any additional keymaps
-                -- for specific lsp servers
-                ---@type LazyKeys[]
-                -- keys = {},
-                settings = {
-                    Lua = {
-                        workspace = {checkThirdParty = false},
-                        completion = {callSnippet = "Replace"}
-                    }
-                }
-            }
-        }
+  "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- change a keymap
+    keys[#keys + 1] = { "<leader>ca", "<cmd>Lspsaga code_action<cr>" }
+    keys[#keys + 1] = { "K", "<cmd>Lspsaga hover_doc<cr>" }
+    keys[#keys + 1] = {
+      "<leader>cr",
+      "<cmd>Lspsaga rename<cr>",
+      desc = "Rename",
     }
+    keys[#keys + 1] = {
+      "gd",
+      "<cmd>Lspsaga goto_definition<cr>",
+      desc = "Goto Definition",
+    }
+    keys[#keys + 1] = {
+      "gy",
+      "<cmd>Lspsaga goto_type_definition<cr>",
+      desc = "Goto Type Definition",
+    }
+  end,
+  ---@class PluginLspOpts
+  opts = {
+    inlay_hints = { enabled = false },
+    ---@type lspconfig.options
+    servers = {
+      jsonls = {},
+      bashls = {},
+      typst_lsp = {},
+      lua_ls = {
+        -- mason = false, -- set to false if you don't want this server to be installed with mason
+        -- Use this to add any additional keymaps
+        -- for specific lsp servers
+        ---@type LazyKeys[]
+        -- keys = {},
+        settings = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            completion = { callSnippet = "Replace" },
+          },
+        },
+      },
+    },
+  },
 }
