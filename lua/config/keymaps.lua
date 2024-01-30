cad998e7b3f6c93e0241ea6c55f033c17b92416f
horaoen <horaoen@gmail.com>
2024-01-30 10:14:34 +0800

    update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,17 @@ local Util = require("lazyvim.util")
 local map = Util.safe_keymap_set
 map("i", "jk", "<ESC>")
 map("n", "<leader>w", "<cmd>w<cr>")
+
+-- Move Lines
+map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<A-l>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<A-h>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
