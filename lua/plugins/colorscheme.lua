0bc4beff907cc4e9d954fa1db04bab966654caea
horaoen <horaoen@gmail.com>
2025-12-14 14:45:04 +0800

    config colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
