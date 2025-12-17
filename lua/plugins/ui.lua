56ca201440af9bd9d34042af40a64fec18649edf
horaoen <horaoen@gmail.com>
2025-12-18 00:23:32 +0800

    add recent files section to alpha dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,7 +4,6 @@ return {
     "goolord/alpha-nvim",
     dependencies = { "nvim-mini/mini.icons" },
     config = function()
-      local alpha = require("alpha")
       local theta = require("alpha.themes.theta")
       theta.header.val = {
         [[                   .@-               ]],
@@ -43,8 +42,32 @@ return {
         position = "center",
       }
 
+      local section_mru = {
+        type = "group",
+        val = {
+          {
+            type = "text",
+            val = "Recent files",
+            opts = {
+              hl = "SpecialComment",
+              shrink_margin = false,
+              position = "center",
+            },
+          },
+          { type = "padding", val = 1 },
+          {
+            type = "group",
+            val = function()
+              return { theta.mru(0, vim.fn.getcwd(), 5) }
+            end,
+            opts = { shrink_margin = false },
+          },
+        },
+      }
       theta.config.layout[6] = buttons
-      alpha.setup(theta.config)
+      theta.config.layout[4] = section_mru
+      theta.opts = theta.config
+      require("alpha").setup(theta.config)
     end,
   },
 }
