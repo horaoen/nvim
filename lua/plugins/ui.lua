f377f54d1f499cc40ae0e539c6cc357267af5138
horaoen <horaoen@gmail.com>
2025-12-17 23:58:16 +0800

    config dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -24,6 +24,26 @@ return {
         [[         :************************@  ]],
         [[         -**++*********************  ]],
       }
+
+      local dashboard = require("alpha.themes.dashboard")
+
+      local buttons = {
+        type = "group",
+        val = {
+          dashboard.button("f", " " .. " Find file", "<cmd> lua LazyVim.pick()() <cr>"),
+          dashboard.button("n", " " .. " New file", [[<cmd> ene <BAR> startinsert <cr>]]),
+          dashboard.button("r", " " .. " Recent files", [[<cmd> lua LazyVim.pick("oldfiles")() <cr>]]),
+          dashboard.button("g", " " .. " Find text", [[<cmd> lua LazyVim.pick("live_grep")() <cr>]]),
+          dashboard.button("c", " " .. " Config", "<cmd> lua LazyVim.pick.config_files()() <cr>"),
+          dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+          dashboard.button("x", " " .. " Lazy Extras", "<cmd> LazyExtras <cr>"),
+          dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+          dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+        },
+        position = "center",
+      }
+
+      theta.config.layout[6] = buttons
       alpha.setup(theta.config)
     end,
   },
