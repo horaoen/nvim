07739e821f5b1418aa5494cef98322da6846c3cc
horaoen <horaoen@gmail.com>
2026-03-01 12:47:51 +0800

    update

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,30 +1,10 @@
 return {
   "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            settings = {
-              java = {
-                format = {
-                  insertSpaces = true,
-                  tabSize = 4,
-                },
-              },
-            },
-          },
-        },
-        setup = {
-          jdtls = function()
-            require("java").setup({})
-          end,
-        },
-      },
-    },
-  },
+  config = function()
+    require("java").setup()
+    vim.lsp.enable("jdtls")
+  end,
+  ft = { "java" },
   keys = {
     { "<leader>fj", "<cmd>:FzfLua lsp_live_workspace_symbols<cr>", desc = "Workspace Symbols Search" },
   },
